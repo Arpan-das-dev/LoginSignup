3f1cc409548494eda6e87ace09f3d5ed195b2549
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-16 14:11:29 +0530

    add repository and dto layers for request and response

--- src/main/java/com/loginSignupAuth/loginSignupAuth/Dto/SignUpRequest.java
+++ src/main/java/com/loginSignupAuth/loginSignupAuth/Dto/SignUpRequest.java
@@ -1,4 +1,16 @@
 package com.loginSignupAuth.loginSignupAuth.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
 }
--- src/main/java/com/loginSignupAuth/loginSignupAuth/Repository/UserRepository.java
+++ src/main/java/com/loginSignupAuth/loginSignupAuth/Repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.loginSignupAuth.loginSignupAuth.Repository;
 
-public interface UserRepository {
+import com.loginSignupAuth.loginSignupAuth.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    boolean existByEmail(String email);
 }
