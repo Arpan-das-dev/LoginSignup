1f3a5813bc3811d5e0100b0794a070084c1c090c
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-16 14:45:21 +0530

    add controller to execute the service logic

--- src/main/java/com/loginSignupAuth/loginSignupAuth/Controller/AuthController.java
+++ src/main/java/com/loginSignupAuth/loginSignupAuth/Controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.loginSignupAuth.loginSignupAuth.Controller;
 
+import com.loginSignupAuth.loginSignupAuth.Dto.AuthResponse;
+import com.loginSignupAuth.loginSignupAuth.Dto.LoginRequest;
+import com.loginSignupAuth.loginSignupAuth.Dto.SignUpRequest;
+import com.loginSignupAuth.loginSignupAuth.Service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+    @PutMapping("/signup")
+    public ResponseEntity<String> signup(SignUpRequest request){
+        authService.signup(request);
+        return ResponseEntity.ok("Successfully Registered");
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
 }
