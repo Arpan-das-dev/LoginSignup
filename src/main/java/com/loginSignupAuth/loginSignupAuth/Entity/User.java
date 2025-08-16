95e993d1c1f404c2f72e67e762353a3c7dc731b9
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-16 14:04:44 +0530

    add the User Entity mapping it into data basee

--- src/main/java/com/loginSignupAuth/loginSignupAuth/Entity/User.java
+++ src/main/java/com/loginSignupAuth/loginSignupAuth/Entity/User.java
@@ -1,4 +1,27 @@
 package com.loginSignupAuth.loginSignupAuth.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "userRegistry")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Email
+    private String email;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String password;
 }
