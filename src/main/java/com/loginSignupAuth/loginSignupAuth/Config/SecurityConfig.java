a86db33d301047349534d713ce039a60c7f34571
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-16 13:34:59 +0530

    add config int the project

--- src/main/java/com/loginSignupAuth/loginSignupAuth/Config/SecurityConfig.java
+++ src/main/java/com/loginSignupAuth/loginSignupAuth/Config/SecurityConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -24,15 +28,34 @@ public class SecurityConfig {
 
             security.
                     csrf(csrf->csrf.disable())
+                    .cors(Customizer.withDefaults())
+                    .sessionManagement(session->session
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authorizeHttpRequests(auth-> auth
-                            .requestMatchers("api/auth/**").permitAll()
+                            .requestMatchers("/api/auth/**").permitAll()
                             .anyRequest().authenticated())
                     .formLogin(Customizer.withDefaults());
             return security.build();
+    }  // adding security filter chain
+
+
+    @Bean
+    public WebMvcConfigurer cors(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET","PUT","POST","DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
+    // using cors policy to access the api url in the frontend
 
     @Bean
     public AuthenticationManager manager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
-    }
+    } // adding an Authentication manager to automatically authenticate the credentials.
 }
